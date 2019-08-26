ae4902500402a10f1ac0f528890b39c11f46a38c
Jijin Sebastian <jijin.sbstn@gmail.com>
2019-08-26 12:20:13 +0530

    Update Boto3_practice.py

--- Boto3_practice.py
+++ Boto3_practice.py
@@ -3,8 +3,8 @@ import boto3
 # client = boto3.client(
 #     's3',
 #     # Hard coded strings as credentials, not recommended.
-#     aws_access_key_id='AKIARVRFM47R6KVLY2JC',
-#     aws_secret_access_key='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'
+#     aws_access_key_id='',
+#     aws_secret_access_key=''
 # )
 # rint s3.buckets.all()
 
